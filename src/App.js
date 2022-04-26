cf77b8c402f3d5f871696ef490d282ea9f6264da
androsv <androsg93@hotmail.com>
2022-04-26 08:39:59 -0500

    uploading app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from 'react'
 
-function App() {
+
+export default function App() {
+  const [data, setData] = useState([])
+  const [filteredPlayers, setFilteredPlayers] = useState([])
+  const [timer, setTimer] = useState(null)
+
+  const onInputChange = ({ currentTarget }) => {
+    if (timer) {
+      clearTimeout(timer)
+      setTimer(null)
+    }
+
+    setTimer(setTimeout(() => {
+      let playersPair = [];
+      let tempCopy = [...data]
+      for (let i = tempCopy.length - 1; i >= 0; i--) {
+        const currPlayer = tempCopy.pop();
+        tempCopy.forEach((player) => {
+          if ((Number(player.h_in) + Number(currPlayer.h_in)) === Number(currentTarget.value)) {
+            playersPair.push([`${player.first_name} ${player.last_name}`, `${currPlayer.first_name} ${currPlayer.last_name}`])
+          }
+        });
+      }
+      setFilteredPlayers(playersPair)
+    }, 500))
+  }
+
+  useEffect(() => {
+    fetch('https://mach-eight.uc.r.appspot.com')
+      .then(res => res.json())
+      .then(res => {
+        if (res.values) {
+          setData(res.values)
+        }
+      })
+      .catch(err => console.log("Ups, something failed.", err))
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input onChange={onInputChange} type="number" placeholder="Player height" />
+      {
+        filteredPlayers.length > 0 ?
+          (
+            <ul>
+              {
+                filteredPlayers.map((playersArr, idx) =>
+                  <li key={`${playersArr[0]}${idx}`}>
+                    <span>{playersArr[0]}</span>
+                    <span>{playersArr[1]}</span>
+                  </li>
+                )
+              }
+            </ul>
+          )
+          :
+          (<h3>Not matches found</h3>)
+      }
     </div>
   );
 }
-
-export default App;
